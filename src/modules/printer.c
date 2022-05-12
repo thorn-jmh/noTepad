8ddaa41f296a4b07a47eebc18d8ef20d06a0ce50
xiaochui <3210105141@zju.edu.cn>
2022-05-12 20:29:33 +0800

    printer 3rd edition

--- src/modules/printer.c
+++ src/modules/printer.c
@@ -6,9 +6,12 @@
 #include <stdlib.h>
 
 #include "file.h"
-DrawCursor(double height){
-    DrawLine(0,height);
+
+int judge(double mouse_x,double mouse_y,double x,double y,double width,double height){
+    if(mouse_x>=x&&mouse_x<=x+width&&mouse_y>=y&&mouse_y<=y+height) return 1;
+    return 0;
 }
+
 void PrintText(string text,int offset,CURSOR_T *crst,MOUSE_T *mst,LINE_T *lnt){
     int isFill=0;                  //是否填充字体
     char ch,str[2];                 //一个字符一个字符地读,由str输出,真就是一个字符一个字符输出
@@ -18,34 +21,37 @@ void PrintText(string text,int offset,CURSOR_T *crst,MOUSE_T *mst,LINE_T *lnt){
     double ch_width,ch_height=TextStringWidth("x")/width*3;          //字体宽度,高度就当整个字符串的高度了,百分比
     double interval=ch_height*1.5;                    //字体间距(含字体),字符串的高度,百分比,我初始化为1.5倍
     double now_width=left;                   //现在输出位置的宽度,百分比
-    int position=0,mouse_position;      //读取的字符在数组的位置,鼠标的位置
+    int position=0,mouse_position=-1;      //读取的字符在数组的位置,鼠标的位置
     lnt->Cline=height*(right-left)/interval;
     int now_line=1;                     //和当前行数
     InitGraphics();
 
     while(ch=text[position++]!='\0'){
+        if(judge(mst->X/width,mst->Y/height,now_width,top-now_line*interval/height,ch_width,ch_height)) mouse_position=position-1;  // 判断mouse的位置
+
         ch_width=TextStringWidth(str);
         str[0]=ch;
         now_width+=ch_width;
-        if(now_line>=lnt->Fline&&now_line<=lnt->Cline+lnt->Fline){                                      //当前行数没超过最大行数，能输出
+        if(now_line>=lnt->Fline&&now_line<lnt->Cline+lnt->Fline){                                      //当前行数没超过最大行数，能输出
             if(ch=='\r'||ch=='\n'){                              //遇到换行符
                 now_line++;
-                now_width=0;
-            }else if(now_width>width*(right-left)){              //遇到边界情况，该字符不输出
+                now_width=left;
+            }else if(now_width>right){              //遇到边界情况，该字符不输出
                 now_line++;
-                now_width=0;
+                now_width=left;
                 position--;                                     //这个是回拨一个position,因为这个字符没输出，所以想让下一次录入仍为这个字符                                 
             }else{                                              //继续走
-                MovePen(width*now_width-ch_width,height*top-now_line*interval);
+                MovePen(width*(now_width-ch_width),height*top-now_line*interval);
                 if(position==crst->PTR_1){
                     if(position==crst->PTR_2){
-                        DrawCursor(ch_height);                  //画光标
+                        crst->X=width*(now_width-ch_width);
+                        crst->Y=height*top-now_line*interval;
                     }else isFill=1;                              //如果两个坐标不同，那就是选中了
                 }
                 if(position==crst->PTR_2) isFill=0;             //结束涂色，PTR_2指向的字符不涂色
                 if(isFill){                                      //将选中的地方涂色
                     SetPenColor("Blue");
-                    drawRectangle(width*now_width-ch_width,height*top-now_line*interval,ch_width,ch_height,1);
+                    drawRectangle(width*(now_width-ch_width),height*top-now_line*interval,ch_width,ch_height,1);
                     SetPenColor("Black");
                 }
                 DrawTextString(str);
@@ -53,10 +59,10 @@ void PrintText(string text,int offset,CURSOR_T *crst,MOUSE_T *mst,LINE_T *lnt){
         }else{                                                //之前的行数，不能输出，只计算
             if(ch=='\r'||ch=='\n'){          
                 now_line++;
-                now_width=0;
+                now_width=left;
             }else if(now_width>width*(right-left)){ 
                 now_line++;
-                now_width=0;
+                now_width=left;
                 position--;                                  
             }else{}
         }
