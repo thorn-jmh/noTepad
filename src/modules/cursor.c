3a40e45b86862a865918849f29a4c2c2e43df9fa
TURING132 <2716153923@qq.com>
2022-05-15 08:40:04 +0800

    renew cursor.c

--- src/modules/cursor.c
+++ src/modules/cursor.c
@@ -4,19 +4,12 @@ static CURSOR_T theCursor ;//定义静态全局变量光标
 
 static MOUSE_T theMouse ;//定义静态全局变量鼠标
 
-static LINE_T theLine;//定义静态全局变量行信息
-
 CURSOR_T *GetCurrentCursor(void){//返回光标指针
     CURSOR_T *pc = &theCursor;
     return pc;
 };
 
-LINE_T *GetCurrentLine(void){//返回鼠标指针
-    LINE_T * pl = &theLine;
-    return pl;
-};
-
-MOUSE_T *GetCurrentMouse(void){//返回行信息指针
+MOUSE_T *GetCurrentMouse(void){//返鼠标指针
     MOUSE_T *pm = &theMouse;
     return pm;
 };
